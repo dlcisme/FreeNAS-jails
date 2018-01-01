fb76aafba5dbe0a27d3ff5179e1b32d6eaed440f
dlcisme <34501019+dlcisme@users.noreply.github.com>
2018-01-01 01:03:16 -0600

    Added code to set sonarr parameters

--- sonarr.sh
+++ sonarr.sh
@@ -3,6 +3,9 @@
 # functions to help installation
 . ./functions.sh
 
+_SONARR_DATA_DIR=/var/local/sonarr
+_SONARR_USER=media
+
 # install sonarr
 Install_Package "sonarr"
 
@@ -13,9 +16,16 @@ Install_Package "sonarr"
 
 #chown -R media:media /usr/local/share/sonarr
 
+echo =====\> set sonarr data dir to $_SONARR_DATA_DIR
+sysrc "sonar_data_dir=$_SONARR_DATA_DIR"
+
+echo =====\> have sonarr run as user $_SONARR_USER
+sysrc "sonarr_user=$_SONARR_USER"
+
 echo =====\> enable sonarr to start at boot
 sysrc  "sonarr_enable=YES"
 
+# no longer needed as we now set sonarr to run as user media
 # add sonarr user to media group
-echo =====\> add "sonarr" user to "media" group
-pw groupmod media -m sonarr
+#echo =====\> add "sonarr" user to "media" group
+#pw groupmod media -m sonarr
