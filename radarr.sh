a91c9ad890e9fa5adc7c0657945bbeb3de79c446
dlcisme <34501019+dlcisme@users.noreply.github.com>
2018-01-07 01:51:16 -0600

    Added rc.conf keywords

--- radarr.sh
+++ radarr.sh
@@ -19,3 +19,21 @@ sysrc "radarr_enable=YES"
 # add radarr user to media group
 echo =====\> add "radarr" user to "media" group
 pw groupmod media -m radarr
+
+
+# Add the following lines to /etc/rc.conf.local or /etc/rc.conf
+# to enable this service:
+#
+# radarr_enable (bool): Set to NO by default.
+#                       Set it to YES to enable it.
+# radarr_data_dir:      Directory where radarr configuration
+#                       data is stored.
+#                       Default: /home/${radarr_user}/.config/NzbDrone
+# radarr_user:  The user account radarr daemon runs as what
+#                       you want it to be. It uses '%%USER%%' user by
+#                       default. Do not sets it as empty or it will run
+#                       as root.
+# radarr_group: The group account radarr daemon runs as what
+#                       you want it to be. It uses '%%GROUP%%' group by
+#                       default. Do not sets it as empty or it will run
+#                       as wheel.
