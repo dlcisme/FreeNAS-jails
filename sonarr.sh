e0728a987b07c85c605a4fbf4049bfda95ff9dd1
dlcisme <34501019+dlcisme@users.noreply.github.com>
2018-01-12 23:46:09 -0600

    Commented out code that changes user Sonarr runs as

--- sonarr.sh
+++ sonarr.sh
@@ -1,36 +1,5 @@
 #!/bin/sh
 
-# functions to help installation
-. ./functions.sh
-
-_SONARR_DATA_DIR=/var/local/sonarr
-_SONARR_USER=media
-
-# install sonarr
-Install_Package "sonarr"
-
-# NOTE:  FreeNAS installs user "Media" and group "Media when
-#        the jail gets created
-# pw groupadd 'media' -g '816'
-# pw useradd 'media' -u '816' -g 'media'
-
-#chown -R media:media /usr/local/share/sonarr
-
-echo =====\> set sonarr data dir to $_SONARR_DATA_DIR
-sysrc "sonar_data_dir=$_SONARR_DATA_DIR"
-
-echo =====\> have sonarr run as user $_SONARR_USER
-sysrc "sonarr_user=$_SONARR_USER"
-
-echo =====\> enable sonarr to start at boot
-sysrc  "sonarr_enable=YES"
-
-# no longer needed as we now set sonarr to run as user media
-# add sonarr user to media group
-#echo =====\> add "sonarr" user to "media" group
-#pw groupmod media -m sonarr
-
-
 # Add the following lines to /etc/rc.conf.local or /etc/rc.conf
 # to enable this service:
 #
@@ -47,3 +16,36 @@ sysrc  "sonarr_enable=YES"
 #                       you want it to be. It uses '%%GROUP%%' group by
 #                       default. Do not sets it as empty or it will run
 #                       as wheel.
+
+
+# functions to help installation
+. ./functions.sh
+
+_SONARR_DATA_DIR=/app-data/Sonarr
+_SONARR_USER=media
+
+# install sonarr
+Install_Package "sonarr"
+
+# NOTE:  FreeNAS installs user "Media" and group "Media when
+#        the jail gets created
+# pw groupadd 'media' -g '816'
+# pw useradd 'media' -u '816' -g 'media'
+
+#chown -R media:media /usr/local/share/sonarr
+
+echo =====\> set sonarr data dir to $_SONARR_DATA_DIR
+sysrc "sonar_data_dir=$_SONARR_DATA_DIR"
+
+#echo =====\> have sonarr run as user $_SONARR_USER
+#sysrc "sonarr_user=$_SONARR_USER"
+
+echo =====\> enable sonarr to start at boot
+sysrc  "sonarr_enable=YES"
+
+# no longer needed as we now set sonarr to run as user media
+# add sonarr user to media group
+#echo =====\> add "sonarr" user to "media" group
+#pw groupmod media -m sonarr
+
+
