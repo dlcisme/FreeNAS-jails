a5a4861e2435f5579b10e7f40bdd6f68faae6c06
dlcisme <34501019+dlcisme@users.noreply.github.com>
2018-01-25 23:08:16 -0600

    Cleaned up code

--- radarr.sh
+++ radarr.sh
@@ -1,25 +1,5 @@
 #!/bin/sh
 
-# functions to help installation
-. ./functions.sh
-
-# install radarr
-Install_Package "radarr"
-
-# NOTE:  FreeNAS installs user "Media" and group "Media when
-#        the jail gets created
-# pw groupadd 'media' -g '816'
-# pw useradd 'media' -u '816' -g 'media'
-
-#chown -R media:media /usr/local/share/sonarr
-
-echo =====\> enable radarr start at boot
-sysrc "radarr_enable=YES"
-
-# add radarr user to media group
-echo =====\> add "radarr" user to "media" group
-pw groupmod media -m radarr
-
 
 # Add the following lines to /etc/rc.conf.local or /etc/rc.conf
 # to enable this service:
@@ -37,3 +17,30 @@ pw groupmod media -m radarr
 #                       you want it to be. It uses '%%GROUP%%' group by
 #                       default. Do not sets it as empty or it will run
 #                       as wheel.
+
+_RADARR_DATA_DIR=/app-data/Radarr
+
+# functions to help installation
+. ./functions.sh
+
+# install radarr
+Install_Package "radarr"
+
+# make radarr the owner of app-data directory
+echo =====\> make radarr owner of $_RADARR_CONFIG_DIR config directory
+chown -R radarr:radarr $_RADARR_DATA_DIR
+
+# NOTE:  FreeNAS installs user "Media" and group "Media when
+#        the jail gets created
+# pw groupadd 'media' -g '816'
+# pw useradd 'media' -u '816' -g 'media'
+
+#chown -R media:media /usr/local/share/radarr
+
+echo =====\> enable radarr start at boot
+sysrc "radarr_enable=YES"
+
+# add radarr user to media group
+echo =====\> add "radarr" user to "media" group
+pw groupmod media -m radarr
+
